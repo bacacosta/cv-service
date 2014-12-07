cfeef5e5ce50d2c5efe18c03cf8092f6f09a590f
baca <bacacosta@yahoo.com.br>
2014-12-07 02:19:03 +0100

    Access-Control-Allow-Origin fixed.

--- src/main/java/com/chuvadasquatro/filter/CORSFilter.java
+++ src/main/java/com/chuvadasquatro/filter/CORSFilter.java
@@ -22,7 +22,7 @@ public class CORSFilter implements Filter {
 	public void doFilter(ServletRequest req, ServletResponse res,
 			FilterChain chain) throws IOException, ServletException {
 		HttpServletResponse response = (HttpServletResponse) res;
-		response.setHeader("Access-Control-Allow-Origin", "http://chuvadasquatro.awardspace.info/");
+		response.setHeader("Access-Control-Allow-Origin", "http://chuvadasquatro.awardspace.info");
 		response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
 		response.setHeader("Access-Control-Max-Age", "3600");
 		response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
