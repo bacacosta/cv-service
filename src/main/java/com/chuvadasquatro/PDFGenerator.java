2c2becfb182cb18193cf6948f14f7681e3772e5d
baca <bacacosta@yahoo.com.br>
2014-12-10 22:20:13 +0100

    Automatic generation of PDF version at startup.

---
+++ src/main/java/com/chuvadasquatro/PDFGenerator.java
@@ -0,0 +1,29 @@
+package com.chuvadasquatro;
+
+import java.io.File;
+import java.io.FileOutputStream;
+
+import org.odftoolkit.odfdom.converter.pdf.PdfConverter;
+import org.odftoolkit.odfdom.converter.pdf.PdfOptions;
+import org.odftoolkit.odfdom.doc.OdfTextDocument;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+
+@SuppressWarnings("deprecation")
+@Component
+public class PDFGenerator implements ApplicationListener<ContextRefreshedEvent> {
+	@Override
+	public void onApplicationEvent(final ContextRefreshedEvent event) {
+		try {
+			new File("public/").mkdir();
+			PdfConverter.getInstance().convert(
+					OdfTextDocument.loadDocument("data/Resume - Rodrigo Costa.odt"),
+					new FileOutputStream("public/Resume - Rodrigo Costa.pdf"),
+					PdfOptions.create()
+			);
+		} catch (Exception e) {
+			throw new RuntimeException("Error converting ODT to PDF.");
+		}
+	}
+}
