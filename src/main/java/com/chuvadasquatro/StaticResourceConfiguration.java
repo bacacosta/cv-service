18be711ccbd759547663601bb6852dab08af4e5a
baca <bacacosta@yahoo.com.br>
2014-12-12 22:25:03 +0100

    Static resource configuration changed.

--- src/main/java/com/chuvadasquatro/StaticResourceConfiguration.java
+++ src/main/java/com/chuvadasquatro/StaticResourceConfiguration.java
@@ -10,6 +10,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 public class StaticResourceConfiguration extends WebMvcConfigurerAdapter {
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("/**").addResourceLocations("file:/diy");
+		registry.addResourceHandler("/**").addResourceLocations("diy");
 	}
 }
