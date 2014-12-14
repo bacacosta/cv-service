3188a835c1e7be52fae3a86350845dd7e94b95ce
baca <bacacosta@yahoo.com.br>
2014-12-15 00:33:30 +0100

    StaticResourceConfiguration restored, logging in PDFGenerator deleted.

--- src/main/java/com/chuvadasquatro/PDFGenerator.java
+++ src/main/java/com/chuvadasquatro/PDFGenerator.java
@@ -16,7 +16,6 @@ public class PDFGenerator implements ApplicationListener<ContextRefreshedEvent> 
 	@Override
 	public void onApplicationEvent(final ContextRefreshedEvent event) {
 		try {
-			System.out.println(this.getClass().getResource("").getPath());
 			final String path = "src/main/resources/public/";
 			final String filename = "Rodrigo-Costa";
 			new File(path).mkdirs();
