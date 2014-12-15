b0c0ea4cb88b83f44baf0a82170f4ab9c9e6446f
Rodrigo Costa <rodrigo.costa@adviqo.com>
2014-12-15 12:28:45 +0100

    Classpath tests.

--- src/main/java/com/chuvadasquatro/App.java
+++ src/main/java/com/chuvadasquatro/App.java
@@ -1,5 +1,8 @@
 package com.chuvadasquatro;
 
+import java.net.URL;
+import java.net.URLClassLoader;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
@@ -11,5 +14,12 @@ import org.springframework.context.annotation.Configuration;
 public class App {
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(App.class, args);
+		ClassLoader cl = ClassLoader.getSystemClassLoader();
+		URL[] urls = ((URLClassLoader) cl).getURLs();
+
+		System.out.println("Classpath: ");
+		for (URL url : urls) {
+			System.out.println(url.getFile());
+		}
 	}
 }
